44fedb94f9eb2953989f75f39fe70eb7da3e8bd1
npomfret <nick-github@snowmonkey.co.uk>
2017-03-13 19:54:51 +0000

    added uninstall script

--- unlink.js
+++ unlink.js
@@ -16,22 +16,35 @@ fs.readFile(location, {encoding: 'utf-8'}, function (err, data) {
     throw new Error(err);
   }
 
-  const commands = ['cd ' + path.resolve(location, "..")];
+  const unlinkCommands = ['cd ' + path.resolve(location, "..")];
+  const uninstallCommands = ['cd ' + path.resolve(location, "..")];
 
   const json = JSON.parse(data);
   for(let name in json.dependencies) {
     if(toKeep.indexOf(name) >= 0) {
       continue;
     }
-    commands.push('react-native unlink ' + name);
+    unlinkCommands.push('react-native unlink ' + name);
+    uninstallCommands.push('npm uninstall ' + name);
   }
 
-  const outputfile = "unlink-packages.sh";
-  fs.writeFile(outputfile, commands.join("\n"), function(err) {
-    if (err) {
-      throw new Error(err);
-    }
+  {
+    const outputfile = "unlink-packages.sh";
+    fs.writeFile(outputfile, unlinkCommands.join("\n"), function(err) {
+      if (err) {
+        throw new Error(err);
+      }
 
-    console.log("run: sh " + outputfile + " && rm " + outputfile);
-  })
+      console.log("run: sh " + outputfile + " && rm " + outputfile);
+    })
+  }
+
+  {
+    const outputfile = "uninstall-packages.sh";
+    fs.writeFile(outputfile, uninstallCommands.join("\n"), function(err) {
+      if (err) {
+        throw new Error(err);
+      }
+    })
+  }
 });
